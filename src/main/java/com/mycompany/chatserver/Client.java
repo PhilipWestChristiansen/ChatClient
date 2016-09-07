d7a6c7945e1f8cc86f83fa29b4d61aeda9b62d01
Casper <Casper@10.50.130.156>
2016-09-07 12:45:32 +0200

    Fikset ting og sager

--- src/main/java/com/mycompany/chatserver/Client.java
+++ src/main/java/com/mycompany/chatserver/Client.java
@@ -27,78 +27,85 @@ public class Client extends Thread {
             Scanner scn = new Scanner(s.getInputStream());
             PrintWriter prnt = new PrintWriter(s.getOutputStream(), true);
             String msg = "";
-            prnt.println("Welcome to the chatroom");
-
+            prnt.println("Please login first with this format: LOGIN:<name>");
             //Login Phase
-            startChat:
-            while (1 == 1) {
+            boolean login = false;
+            while (!login) {
                 msg = scn.nextLine();
                 if (msg.contains("LOGIN:")) {
-                    //{ "LOGIN" , "Casper" }
                     String[] parts = msg.split(":");
                     user = new Username(parts[1]);
-                    userList.add(user);
-                    System.out.println("Added user: " + parts[1]);
-                    prnt.println(showClients());
-                    break startChat;
+                    if (!userList.contains(user)) {
+                        userList.add(user);
+                        prnt.println(showClients());
+                        login = true;
+                    } else {
+                        prnt.println("User already exists");
+                        login = false;
+                    }
                 }
             }
 
             //Chatting phase
-            while (!msg.contains("LOGOUT")) {
-                msg = scn.nextLine();
+            if (login) {
+                prnt.println("Welcome to the chatroom");
+                while (!msg.contains("LOGOUT")) {
+                    msg = scn.nextLine();
 
-                //If the user tries to login again.
-                if (msg.contains("LOGIN:")) {
-                    prnt.println("You're already logged in as " + user.getUsername());
-                }
-                
-                //To ensure that msg does not only contain these keywords.
-                if (msg.equals("MSG:") || msg.equals("LOGIN:")) {
-                    prnt.println("Please specify your command");
-                    continue;
-                }
+                    //If the user tries to login again.
+                    if (msg.contains("LOGIN:")) {
+                        prnt.println("You're already logged in as " + user.getUsername());
+                    }
 
-                //Message Part
-                if (msg.contains("MSG:")) {
-                    //MSG:Casper,Philip,Hamza:Hej
-                    //parts array: { "MSG" , "Casper,Philip,Hamza" , "Hej"}
-                    String[] parts = msg.split(":");
+                    //To ensure that msg does not only contain these keywords.
+                    if (msg.equals("MSG:") || msg.equals("LOGIN:")) {
+                        prnt.println("Please specify your command");
+                        continue;
+                    }
 
-                    //clients array: { "Casper" , "Hamza" , "Philip"}
-                    String[] clients = parts[1].split(",");
+                    //Message Part
+                    if (msg.contains("MSG:")) {
+                        //MSG:Casper,Philip,Hamza:Hej
+                        //parts array: { "MSG" , "Casper,Philip,Hamza" , "Hej"}
+                        String[] parts = msg.split(":");
 
-                    //Check if client exists in our list of clients
-                    for (int i = 0; i < clients.length; i++) {
-                        String client = clients[i];
-                        for (int j = 0; j < userList.size(); j++) {
-                            if (userList.get(i).getUsername().equals(client)) {
-                                //Insert method here that sends a message to that specific user
-                                //Example method: send(String client, String message);
-                                //message = parts[2];
-                                continue;
+                        //clients array: { "Casper" , "Hamza" , "Philip"}
+                        String[] clients = parts[1].split(",");
+
+                        //Check if client exists in our list of clients
+                        for (int i = 0; i < clients.length; i++) {
+                            String client = clients[i];
+                            for (int j = 0; j < userList.size(); j++) {
+                                if (userList.get(i).getUsername().equals(client)) {
+                                    //Insert method here that sends a message to that specific user
+                                    //Example method: send(String client, String message);
+                                    //message = parts[2];
+                                    continue;
+                                }
                             }
+                        }
+
+                    }
+
+                    //Message ALL part
+                    if (msg.contains("MSG::")) {
+                        String[] parts = msg.split(":");
+                        for (Username user : userList) {
+                            //send(client.getUser, parts[2]);
                         }
                     }
 
-                }
-
-                //Message ALL part
-                if (msg.contains("MSG::")) {
-                    String[] parts = msg.split(":");
-                    for (Username user : userList) {
-                        //send(client.getUser, parts[2]);
+                    //Get list of clients
+                    if (msg.equals("CLIENTLIST:")) {
+                        prnt.println(showClients());
                     }
-                }
 
-                //Get list of clients
-                if (msg.equals("CLIENTLIST:")) {
-                    prnt.println(showClients());
                 }
-
             }
+
             //REMOVE CLIENT FROM CLIENTLIST
             userList.remove(user);
+            System.out.println("Removed " + user.getUsername() + " from the list of clients");
 
             //Close Connection
             scn.close();
@@ -108,12 +115,12 @@ public class Client extends Thread {
             System.out.println(e.getMessage());
         }
     }
-
     //Show list of clients
+
     public String showClients() {
         String users = "CLIENTLIST:";
-        for (Username user : userList) {
-            users += user.getUsername() + ",";
+        for (Username username : userList) {
+            users += username.getUsername() + ",";
         }
         return users;
     }
